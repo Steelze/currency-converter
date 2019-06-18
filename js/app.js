b1d94437278af06069631887b7664296a4824117
Steelze <sogungbure@gmail.com>
2019-06-18 10:49:43 +0100

    Create app  markup

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// document.querySelector('#amount').classList.add('field-error');
+document.querySelector('#error-msg').classList.add('d-none');
+document.querySelector('#processing').classList.add('d-none');
+// document.querySelector('#result').classList.add('d-none');
+// document.querySelector('#info').classList.add('d-none');
