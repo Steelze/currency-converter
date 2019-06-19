9677e87b6d5d05d56b18fefcc380d5075818ebee
Steelze <sogungbure@gmail.com>
2019-06-19 09:33:56 +0100

    Append currencies from API to select tag

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-// const api_key = '98b713dc18f115197d07';
-const api_key = 'uiy23478q5q';
+const api_key = '98b713dc18f115197d07';
 const base_url = `https://free.currconv.com/api/v7/`;
 const convert_url = `${base_url}convert?q=USD_PHP&compact=ultra&apiKey=${api_key}`;
 const currencies_url = `${base_url}currencies?apiKey=${api_key}`
 
+const form = document.querySelector('form');
 const amount_field = document.querySelector('#amount');
 const amount_error_div = document.querySelector('#amount-error em');
 const currency_field_1 = document.querySelector('#currency1');
@@ -13,6 +13,7 @@ const processing_div_text = document.querySelector('#processing strong');
 const result_div = document.querySelector('#result');
 const info_div = document.querySelector('#info');
 const submit_button = document.querySelector('#submit');
+const app_error_field = document.querySelector('#error-msg');
 
 function disableSubmitButton() {
     submit_button.disabled = true;
@@ -20,6 +21,11 @@ function disableSubmitButton() {
 
 function enableSubmitButton() {
     submit_button.disabled = false;
+}
+
+function hideApplicationError() {
+    app_error_field.classList.add('d-none');
+    app_error_field_text.textContent = '';
 }
 
 function hideProcessingDiv() {
@@ -37,6 +43,12 @@ function removehighlightFromField() {
     amount_field.classList.remove('field-error');
     amount_field.nextSibling.nextSibling.classList.add('d-none');
     amount_error_div.textContent = ''
+}
+
+function showApplicationError(text = 'Oops... Something went wrong') {
+    hideProcessingDiv();
+    app_error_field.classList.remove('d-none');
+    app_error_field.textContent = text;
 }
 
 function showProcessingDiv(text = 'Processing...') {
@@ -59,27 +71,46 @@ amount_field.addEventListener('keyup', _ => {
     }
 });
 
+function createOptionTag(text = '', value = '') {
+    const option = document.createElement("option");
+    option.text = text;
+    option.value = value;
+    return option;
+}
+function appendCurrenciesToSelect(currencies){
+    const entries = Object.values(currencies)
+    for (const currency of entries) {
+        currency_field_1.appendChild(createOptionTag(currency.currencyName, currency.id));
+        currency_field_2.appendChild(createOptionTag(currency.currencyName, currency.id));
+    }
+}
 
-function fetchCurrency() {
-    fetch(currencies_url)
+function fetchCurrencies() {
+    return fetch(currencies_url)
     .then(function(response) {
         return response.json();
-    })
-    .then((datas) => {
-        console.log(datas);
-        
-    })
-    .catch(e => console.log('Error ooo', e));
+    }).then( data => {
+        if (data.status === 400) {
+            throw new Error(data.error);
+        }
+        return data;
+    });
 }
+
 
 const app = async _ => {
-    console.log('Initializing App');
     showProcessingDiv('Starting Application...');
-    // fetchCurrency().then(e => console.log(e)).catch(r => console.log('Error ooo', r));
-    fetchCurrency();
+    fetchCurrencies()
+        .then(data => {
+            appendCurrenciesToSelect(data.results);
+        })
+        .catch(error => {
+            const error_msg = error.message.split('.')[0];
+            showApplicationError(error_msg);
+        });
 }
 
-app().then(_ => console.log('After initiallizing'));
+app().then(_ => hideProcessingDiv());
 
 //Register service worker
 // if ('serviceWorker' in navigator) {
